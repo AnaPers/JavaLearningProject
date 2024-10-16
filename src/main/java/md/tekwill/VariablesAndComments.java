b2471ac23b0bc05df5c6139a8dacc41ec98100d4
AnaPers <kokosik.ana@gmail.com>
2024-10-16 23:26:57 +0100

    Added the variables and comments task

---
+++ src/main/java/md/tekwill/VariablesAndComments.java
@@ -0,0 +1,48 @@
+package md.tekwill;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+
+      //Declare section
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDatType;
+        byte upperLimitOfByteDatType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDatType;
+        double loweLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+        // Initialization of the above variables
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 'A';
+        lastCharFromAscii = 'Z';
+        lowerLimitOfByteDatType = -128;
+        upperLimitOfByteDatType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -214748836648;
+        upperLimitOfIntDataType = 214748836647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+38F;
+        upperLimitOfFloatDatType = 3.4E+38F;
+        loweLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+305;
+
+
+
+    }
+}
